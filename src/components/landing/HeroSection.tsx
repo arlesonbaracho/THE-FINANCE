57b5aef827ff5a5477cba6d695e8dd092506ad13
Arleson Baracho <arlesonrodrigues35@gmail.com>
2026-05-31 13:51:09 -0300

    fix(landing): point mascot to /Mascot.png (transparent background), remove SVG filter

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -186,29 +186,16 @@ function Mascot() {
         animation: 'lp-ring-pulse 3.5s ease-in-out infinite 1.75s',
         pointerEvents: 'none',
       }} />
-      {/* SVG filter to remove white background */}
-      <svg width="0" height="0" style={{ position: 'absolute' }} aria-hidden>
-        <defs>
-          <filter id="lp-remove-white">
-            <feColorMatrix type="matrix" values="
-              1 0 0 0 0
-              0 1 0 0 0
-              0 0 1 0 0
-              -2 -2 -2 0 4.6
-            " />
-          </filter>
-        </defs>
-      </svg>
       {/* imagem */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
-        src="/mascot.svg"
+        src="/Mascot.png"
         alt="The Finance mascot"
         style={{
           width: 'clamp(320px, 40vw, 560px)',
           position: 'relative', zIndex: 2,
           animation: 'lp-float 5s ease-in-out infinite',
-          filter: 'url(#lp-remove-white) drop-shadow(0 0 60px rgba(74,222,128,0.55)) drop-shadow(0 24px 48px rgba(0,0,0,0.7))',
+          filter: 'drop-shadow(0 0 60px rgba(74,222,128,0.55)) drop-shadow(0 24px 48px rgba(0,0,0,0.7))',
           userSelect: 'none', pointerEvents: 'none',
         }}
       />
